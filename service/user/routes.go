d0a93ad532e61211768f77dbbd2aa2f88765d6ba
RobTov <tovelrob25@gmail.com>
2024-06-25 21:53:16 -0400

    added get users handler

--- service/user/routes.go
+++ service/user/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/RobTov/hmblog-golang-backend/types"
+	"github.com/RobTov/hmblog-golang-backend/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -16,9 +17,19 @@ func NewHandler(store types.UserStore) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
-	router.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {}).Methods(http.MethodGet)
+	router.HandleFunc("/users", h.handleGetUsers).Methods(http.MethodGet)
 	router.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {}).Methods(http.MethodPost)
 	router.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {}).Methods(http.MethodPost)
 	router.HandleFunc("/user/{id}", func(w http.ResponseWriter, r *http.Request) {}).Methods(http.MethodPut)
 	router.HandleFunc("/user/{id}", func(w http.ResponseWriter, r *http.Request) {}).Methods(http.MethodDelete)
+}
+
+func (h *Handler) handleGetUsers(w http.ResponseWriter, r *http.Request) {
+	users, err := h.store.GetUsers()
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, users)
 }
